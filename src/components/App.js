c837ede92305ee2d13574a79bfa77273337b6ca5
신은정 <ejshin0213@naver.com>
2022-05-31 15:35:41 +0900

    나만의 Hook으로 코드 정리하기

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { createReview, deleteReview, updateReview } from "../api";
 import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 import ReviewForm from "./ReviewForm";
+import useAsync from "./hooks/useAsync";
 
 const LIMIT = 6;
 
@@ -11,8 +12,7 @@ function App() {
   const [order, setOrder] = useState("createdAt");
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadingError, setLoadingError] = useState(null);
+  const [isLoading, loadingError, getReviewsAsync] = useAsync(getReviews);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -25,17 +25,9 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    let result;
-    try {
-      setIsLoading(true);
-      setLoadingError(null);
-      result = await getReviews(options);
-    } catch (error) {
-      setLoadingError(error);
-      return;
-    } finally {
-      setIsLoading(false);
-    }
+    const result = await getReviewsAsync(options);
+    if (!result) return;
+
     const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import FileInput from "./FileInput";
+import useAsync from "./hooks/useAsync";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
 
@@ -18,8 +19,7 @@ function ReviewForm({
   onCancel,
   onSubmit,
 }) {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submittingError, setSubmittingError] = useState(null);
+  const [isSubmitting, submittingError, onSubmitAsync] = useAsync(onSubmit);
   const [values, setValues] = useState(initialValues);
 
   const handleChange = (name, value) => {
@@ -42,17 +42,8 @@ function ReviewForm({
     formData.append("content", values.content);
     formData.append("imgFile", values.imgFile);
 
-    let result;
-    try {
-      setSubmittingError(null);
-      setIsSubmitting(true);
-      result = await onSubmit(formData);
-    } catch (error) {
-      setSubmittingError(error);
-      return;
-    } finally {
-      setIsSubmitting(false);
-    }
+    const result = await onSubmitAsync(formData);
+    if (!result) return;
     const { review } = result;
     setValues(INITIAL_VALUES);
     onSubmitSuccess(review);
