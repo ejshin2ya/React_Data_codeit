3d9a61eef565336c61a7a158331d97b8357eb539
신은정 <ejshin0213@naver.com>
2022-06-02 00:22:49 +0900

    Context 코드 분리하기

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,12 @@ import { useCallback, useEffect, useState } from "react";
 import { getReviews } from "../api";
 import ReviewForm from "./ReviewForm";
 import useAsync from "./hooks/useAsync";
-import LocaleContext from "./contexts/LocaleContext";
+import { LocaleProvider } from "./contexts/LocaleContext";
 import LocaleSelect from "./LocaleSelect";
 
 const LIMIT = 6;
 
 function App() {
-  const [locale, setLocale] = useState("ko");
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const [offset, setOffset] = useState(0);
@@ -68,9 +67,9 @@ function App() {
   }, [order, handleLoad]);
 
   return (
-    <LocaleContext.Provider value={locale}>
+    <LocaleProvider defaultValue={"ko"}>
       <div>
-        <LocaleSelect value={locale} onChange={setLocale} />
+        <LocaleSelect />
         <div>
           <button onClick={handleNewestClick}>최신순</button>
           <button onClick={handleBestClick}>베스트순</button>
@@ -92,7 +91,7 @@ function App() {
         )}
         {loadingError?.message && <span>{loadingError.message}</span>}
       </div>
-    </LocaleContext.Provider>
+    </LocaleProvider>
   );
 }
 
